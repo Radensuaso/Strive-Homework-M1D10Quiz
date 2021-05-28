6ad5c9f5e0561ab6afd9bca3c065b96562d089cc
Radensuaso <andrels9283@gmail.com>
2021-05-28 11:31:41 +0100

    appended the question-containers to the main-question-container

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const questions = [
     type: "multiple",
     difficulty: "easy",
     question:
-      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
@@ -94,15 +94,57 @@ const questions = [
   },
 ]
 
+const mainQuestionsContainer = document.querySelector(
+  "#main-questions-container"
+)
+
 window.onload = function () {
-  // IF YOU ARE DISPLAYING ALL THE QUESTIONS AT ONCE:
-  // HINT: for each question, create a container with the "question"
-  // create a radio button https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio with, as option the both the correct answer and the incorrect answers
-  // when EVERY question has an answer (google for how to get a value from a radio button with JS)
-  // IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
-  // Display first question with a title + radio button
-  // when the user select the answer, pick the next question and remove this from the page after added in a varible the users' choice.
+  createQuestionContainers()
 }
+
+// Function to create every question container
+const createQuestionContainers = () => {
+  for (const questionObj of questions) {
+    const questionContainer = document.createElement("div") // creating an element for every question in the questions object
+    questionContainer.classList.add("question-containers") // adding a class to every container
+
+    mainQuestionsContainer.appendChild(questionContainer)
+  }
+
+  /* const questionContainer = document.querySelectorAll(".question-container")
+  
+
+  for (const questionObj of questions) {
+    const categoryH3 = (document.createElement("h3").innerText =
+      questionObj.category) // creating h3 element with category of the question
+    const questionH2 = (document.createElement("h2").innerText =
+      questionObj.question) // creating h2 element with the question
+
+    questionContainer.appendChild(categoryH3)
+    questionContainer.appendChild(questionH2)
+  }*/
+}
+
+// variable that holds the question-containers array
+
+const questionContainers = document.querySelectorAll(".question-containers")
+
+console.log(questionContainers)
+
+// function to append the questions to the containers
+/* const appendQuestionsToTheContainers = () => {}
+
+// function to display one question at a time
+
+const displayQuestion = () => {} */
+
+// IF YOU ARE DISPLAYING ALL THE QUESTIONS AT ONCE:
+// HINT: for each question, create a container with the "question"
+// create a radio button https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio with, as option the both the correct answer and the incorrect answers
+// when EVERY question has an answer (google for how to get a value from a radio button with JS)
+// IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
+// Display first question with a title + radio button
+// when the user select the answer, pick the next question and remove this from the page after added in a varible the users' choice.
 
 // HOW TO calculate the result
 // You can do it in 2 ways:
